744be5404c831b911a336d852f9ff270bb96eaed
Nataly-Kuzminska <kuzminskanataly@gmail.com>
2021-06-22 19:32:08 +0300

    create: component Modal

--- src/Components/ModalItem.js
+++ src/Components/ModalItem.js
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 
 const Overlay= styled.div`
 position: fixed;
+display: flex;
+justify-content: center;
+align-items: center;
 top: 0;
 left: 0;
 width: 100%;
@@ -10,9 +13,14 @@ height: 100%;
 background-color: rgba(0, 0, 0, .5);
 z-index: 20; 
 `;
+const Modal = styled.div`
+background-color: #fff;
+width: 600px;
+height: 600px;
+`;
 
 export const ModalItem= () => (
 <Overlay>
-
+<Modal>ТЕСТ</Modal>
 </Overlay>
 )
