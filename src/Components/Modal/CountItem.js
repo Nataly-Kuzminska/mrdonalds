0e13d496077f9d404b84fdffbae57c729471193b
Nataly-Kuzminska <kuzminskanataly@gmail.com>
2021-07-13 14:57:21 +0300

    create: Hook useCount

--- src/Components/Modal/CountItem.js
+++ src/Components/Modal/CountItem.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
+const CountWrapper = styled.div``;
+
+const CountInput = styled.input``;
 
 export function CountItem() {
   return <div>Количество</div>
